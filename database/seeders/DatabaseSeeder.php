5a9cfb611d503fcba1ae04e9d8d55454be3c8514
Gilbert <gilbert.baracka@gmail.com>
2023-01-04 09:56:01 -0700

    seeding data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Product;
+use App\Models\Media;
 use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
@@ -28,8 +29,9 @@ class DatabaseSeeder extends Seeder
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
         
         {
-        for($i = 0; $i <= 3;$i++ ){
-        Product::create(['product_name' => $faker->productName]);
+        for($i = 0; $i <= 1;$i++ ){
+        $product = Product::create(['product_name' => $faker->productName]);
+        $media = Media::create(['product_id'=> $product->product_id]);
         }
         // DB::table('product')->insert([
         //     'product_name' => $faker->productName,
